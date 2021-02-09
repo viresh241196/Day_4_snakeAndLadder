c59491eaf306335a2561a72bd17e9bf02b63a5db
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 21:31:08 +0530

    added: logic for noplay snake or ladder

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,18 @@
 public class SnakeAndLadder {
+    public static final int ladder = 2;
+    public static final int snake = 1;
 
     public static void main(String[] args) {
+        int position=0;
+        int player=0;
         int dice = (int)Math.floor(Math.random() * 10 ) % 6+1;
-        System.out.println("dice output ="+dice);
+        int option = (int)Math.floor(Math.random() * 10 ) % 3;
+        if (option==ladder)
+            position+=dice;
+        else if (option==snake)
+            position-=dice;
+        System.out.println("dice output : "+dice);
+        System.out.println("option output : "+option);
+        System.out.println("position output : "+position);
     }
 }
