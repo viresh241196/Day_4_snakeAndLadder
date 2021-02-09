eb289c325bf4b5cf8f6c6e0deccbdcd6c6bfd5f2
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 21:38:22 +0530

    modified:the end to reach exact 100 position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,21 +1,26 @@
 public class SnakeAndLadder {
     public static final int ladder = 2;
     public static final int snake = 1;
-
     public static void main(String[] args) {
         int position = 0;
         int player = 0;
         while (position <= 100) {
             int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
             int option = (int) Math.floor(Math.random() * 10) % 3;
-            if (option == ladder)
-                position += dice;
-            else if (option == snake)
-                position -= dice;
-            if (position < 0)
-                position = 0;
-            System.out.println("dice output : " + dice);
-            System.out.println("option output : " + option);
+            if (option == ladder) {
+                if (position + dice <= 100) {
+                    position += dice;
+                }
+            } else if (option == snake) {
+                if (position - dice >= 0) {
+                    position -= dice;
+                } else {
+                    position = 0;
+                }
+                System.out.println("dice output : " + dice);
+                System.out.println("option output : " + option);
+                System.out.println("position output : " + position);
+            }
             System.out.println("position output : " + position);
         }
     }
