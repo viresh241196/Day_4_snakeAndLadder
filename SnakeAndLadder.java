373bfaa14efd380df7429a3d4c87c3df4aaa3123
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 21:27:07 +0530

    modified:to create the logic of dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,7 @@
 public class SnakeAndLadder {
-    public static void main(String[] args) {
-        System.out.println("welcome to snake and ladder");
-    }
 
+    public static void main(String[] args) {
+        int dice = (int)Math.floor(Math.random() * 10 ) % 6+1;
+        System.out.println("dice output ="+dice);
+    }
 }
