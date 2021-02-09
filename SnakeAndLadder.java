5704667c42365d51459e2212c38a890fbdd0017d
viresh rawool <vireshrawool007@gmail.com>
2021-02-09 23:50:22 +0530

    modified:to calculate two player game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,28 +2,63 @@ public class SnakeAndLadder {
     public static final int ladder = 2;
     public static final int snake = 1;
     public static void main(String[] args) {
-        int position = 0;
-        int counter = 0;
-        while (position<100) {
-            int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        int player1 = 0;
+        int player2 = 0;
+        int checker =1;
+        while (true) {
+            if (player1==100 || player2==100)
+                break;
+            int dice1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
             int option = (int) Math.floor(Math.random() * 10) % 3;
-            switch (option){
-                case ladder:
-                if (position + dice <= 100) {
-                    position += dice;
+            switch (checker){
+                case 1:
+                switch (option) {
+                    case ladder:
+                        checker=1;
+                        if (player1 + dice1 <= 100) {
+                            player1 += dice1;
+                        }
+                        break;
+                    case snake:
+                        if (player1 - dice1 >= 0) {
+                            player1 -= dice1;
+                        } else {
+                            player1 = 0;
+                        }
+                        checker=2;
+                        break;
+                    default:
+                        checker=2;
+
                 }
-                break;
-                case snake:
-                if (position - dice >= 0) {
-                    position -= dice;
-                } else {
-                    position = 0;
-                }
-                break;
+                case 2:
+                    switch (option) {
+                        case ladder:
+                            checker=2;
+                            if (player2 + dice1 <= 100) {
+                                player2 += dice1;
+                            }
+                            break;
+                        case snake:
+                            if (player2 - dice1 >= 0) {
+                                player2 -= dice1;
+                            } else {
+                                player2 = 0;
+                            }
+                            checker=1;
+                            break;
+                        default:
+                            checker=1;
+                    }
             }
-            counter++;
-            System.out.println("position output : " + position);
         }
-        System.out.println("dice was roll for : " + counter + " times");
+        if(player1 ==100)
+            System.out.println("player1 won"+player1+" "+player2);
+        else
+            System.out.println("player2 won"+player1+" "+player2);
     }
 }
+
+
+
+
