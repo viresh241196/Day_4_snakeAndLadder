b59891663644d6bcae9f2febaeb935362c099b54
viresh rawool <vireshrawool007@gmail.com>
2021-02-10 22:56:58 +0530

    modified: added output statements

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,7 @@
 public class SnakeAndLadder {
     public static final int ladder = 2;
     public static final int snake = 1;
+    public static final int no_pay = 0;
     public static void main(String[] args) {
         int player1 = 0;
         int player2 = 0;
@@ -12,25 +13,27 @@ public class SnakeAndLadder {
             int option = (int) Math.floor(Math.random() * 10) % 3;
             switch (checker){
                 case 1:
-                switch (option) {
-                    case ladder:
-                        checker=1;
-                        if (player1 + dice1 <= 100) {
-                            player1 += dice1;
-                        }
-                        break;
-                    case snake:
-                        if (player1 - dice1 >= 0) {
-                            player1 -= dice1;
-                        } else {
-                            player1 = 0;
-                        }
-                        checker=2;
-                        break;
-                    default:
-                        checker=2;
-
-                }
+                    switch (option) {
+                        case ladder:
+                            checker=1;
+                            if (player1 + dice1 <= 100) {
+                                player1 += dice1;
+                            }
+                            break;
+                        case snake:
+                            if (player1 - dice1 >= 0) {
+                                player1 -= dice1;
+                            } else {
+                                player1 = 0;
+                            }
+                            checker=2;
+                            break;
+                        default:
+                            checker=2;
+                    }
+                    System.out.println("dice outcome "+dice1);
+                    System.out.println("option case outcome "+option);
+                    System.out.println("player1 position "+player1);
                 case 2:
                     switch (option) {
                         case ladder:
@@ -50,15 +53,14 @@ public class SnakeAndLadder {
                         default:
                             checker=1;
                     }
+                    System.out.println("dice outcome "+dice1);
+                    System.out.println("option case outcome "+option);
+                    System.out.println("player2 position "+player2);
             }
         }
         if(player1 ==100)
-            System.out.println("player1 won"+player1+" "+player2);
+            System.out.println("player1 won");
         else
-            System.out.println("player2 won"+player1+" "+player2);
+            System.out.println("player2 won");
     }
 }
-
-
-
-
