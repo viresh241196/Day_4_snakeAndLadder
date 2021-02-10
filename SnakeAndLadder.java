51c30d3d5651d946cab76938fedfda0573292b30
viresh rawool <vireshrawool007@gmail.com>
2021-02-10 23:06:24 +0530

    modified: indentation

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,20 +2,21 @@ public class SnakeAndLadder {
     public static final int ladder = 2;
     public static final int snake = 1;
     public static final int no_pay = 0;
+
     public static void main(String[] args) {
         int player1 = 0;
         int player2 = 0;
-        int checker =1;
+        int checker = 1;
         while (true) {
-            if (player1==100 || player2==100)
+            if (player1 == 100 || player2 == 100)
                 break;
             int dice1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
             int option = (int) Math.floor(Math.random() * 10) % 3;
-            switch (checker){
+            switch (checker) {
                 case 1:
                     switch (option) {
                         case ladder:
-                            checker=1;
+                            checker = 1;
                             if (player1 + dice1 <= 100) {
                                 player1 += dice1;
                             }
@@ -26,18 +27,18 @@ public class SnakeAndLadder {
                             } else {
                                 player1 = 0;
                             }
-                            checker=2;
+                            checker = 2;
                             break;
                         default:
-                            checker=2;
+                            checker = 2;
                     }
-                    System.out.println("dice outcome "+dice1);
-                    System.out.println("option case outcome "+option);
-                    System.out.println("player1 position "+player1);
+                    System.out.println("dice outcome " + dice1);
+                    System.out.println("option case outcome " + option);
+                    System.out.println("player1 position " + player1);
                 case 2:
                     switch (option) {
                         case ladder:
-                            checker=2;
+                            checker = 2;
                             if (player2 + dice1 <= 100) {
                                 player2 += dice1;
                             }
@@ -48,17 +49,17 @@ public class SnakeAndLadder {
                             } else {
                                 player2 = 0;
                             }
-                            checker=1;
+                            checker = 1;
                             break;
                         default:
-                            checker=1;
+                            checker = 1;
                     }
-                    System.out.println("dice outcome "+dice1);
-                    System.out.println("option case outcome "+option);
-                    System.out.println("player2 position "+player2);
+                    System.out.println("dice outcome " + dice1);
+                    System.out.println("option case outcome " + option);
+                    System.out.println("player2 position " + player2);
             }
         }
-        if(player1 ==100)
+        if (player1 == 100)
             System.out.println("player1 won");
         else
             System.out.println("player2 won");
